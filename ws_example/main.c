7b7d6ebdc88eb7a1a08083f0704e3594bd67bc05
nik1998 <beregnovnikita@gmail.com>
2017-12-24 02:03:39 +0300

    empty

--- ws_example/main.c
+++ ws_example/main.c
@@ -1,79 +1,13 @@
 #include "main.h"
-#include "libARM/libARM.h"i
+#include "libARM/libARM.h"
 
 #define SWITCH_DELAY 500000
-#define LED1_PIN GPIO_Pin_12
-#define LED2_PIN GPIO_Pin_13
-#define LED3_PIN GPIO_Pin_14
-#define LED4_PIN GPIO_Pin_15
+
 
 
 int main(void)
 {
- // GPIO_InitTypeDef  GPIO_InitStructure;
-
-  /* Enable peripheral clock for GPIOD port */
- // RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
- /* RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
-
-  // RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-  // SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE,EXTI_PinSource0);
-
-  //Init Leds 
-  GPIO_PinAFConfig(GPIOA,GPIO_PinSource8,GPIO_AF_TIM1);*/
-//GPIO_InitStructure.GPIO_Pin= GPIO_Pin_8/*|GPIO_Pin_9|GPIO_Pin_10*/;
- /* GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
- 
-  //Light
-  TIM_TimeBaseInitTypeDef ttt;
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1,ENABLE);
-  ttt.TIM_Period=100000/60-1;
-  ttt.TIM_Prescaler=1680;
-  ttt.TIM_ClockDivision=0;
-  ttt.TIM_CounterMode=TIM_CounterMode_Up;
-  TIM_TimeBaseInit(TIM1,&ttt);
-  TIM_CtrlPWMOutputs(TIM1,ENABLE);
-  TIM_Cmd(TIM1,ENABLE);
-
-  TIM_OCInitTypeDef aaa;
-  aaa.TIM_OCMode=TIM_OCMode_PWM1;
-  aaa.TIM_Pulse=1600;
-  aaa.TIM_OCPolarity=TIM_OCPolarity_High; 
-  TIM_OC1Init(TIM1,&aaa);
-  TIM_OC1PreloadConfig(TIM1,TIM_OCPreload_Enable);*/
-
- //button
- /*GPIO_InitTypeDef GPIO_In;
-GPIO_In.GPIO_Pin= GPIO_Pin_1|GPIO_Pin_0;
-  GPIO_In.GPIO_Mode = GPIO_Mode_IN;
-  GPIO_In.GPIO_OType = GPIO_OType_PP;
-  GPIO_In.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_In.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_Init(GPIOE, &GPIO_In);*/
- //interrupt
-  /* EXTI_InitTypeDef eee;
-   eee.EXTI_Line=EXTI_Line0;
-   eee.EXTI_LineCmd=ENABLE;
-   eee.EXTI_Mode=EXTI_Mode_Interrupt;
-   eee.EXTI_Trigger=EXTI_Trigger_Rising;
-   EXTI_Init(&eee);*/
-
-   //vector
- /* NVIC_InitTypeDef nvec;
-  nvec.NVIC_IRQChannel=EXTI0_IRQn;
-  nvec.NVIC_IRQChannelPreemptionPriority=0x00;
-  nvec.NVIC_IRQChannelSubPriority=0x01;
-  nvec.NVIC_IRQChannelCmd=ENABLE;
-  NVIC_Init(&nvec);*/
-
-  //int index
- // int ok=1;
-   InitLeds(0x0);
+    InitLeds(0x0);
    InitTIM1andPWM();
    int  c=1665;
    int  h=1;
